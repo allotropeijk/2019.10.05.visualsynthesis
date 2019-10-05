1db89bdf02cbc00df8622827487b72532967d305
chrisfromwork <c.m.barth@outlook.com>
2019-10-05 15:57:19 -0700

    add demo stuff

---
+++ 2019.10.05.visualsynthesis/Assets/Demo/DemoMaterialHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoMaterialHelper : MonoBehaviour
+{
+    [SerializeField]
+    Material demoMaterial;
+
+    [SerializeField]
+    float scale = 10.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        demoMaterial.SetFloat("_TimeScale", scale);
+    }
+
+    public void SetScale(float value)
+    {
+        scale = Mathf.Lerp(1.0f, 1000, value);
+    }
+}
